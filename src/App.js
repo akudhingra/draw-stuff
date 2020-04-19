c89d911d7fd9182f5c57c4bfbe830b0b8e716769
Aakarshit Dhingra <dhingraa@gmail.com>
2020-04-19 07:35:47 -0700

    Made stroke animation configurable

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
 import React from 'react';
-import { Button } from '@material-ui/core';
+import { Button, ButtonGroup } from '@material-ui/core';
 import uppercaseCharacters from './uppercase.json';
 import 'typeface-roboto';
 import './App.css';
+
+
+const DefaultStrokeSettings = {
+  stroke0: {
+    dasharray: 400,
+    dashoffset: 400,
+    durationMs: 1000,
+    delayMs: 200
+  },
+  stroke1: {
+    dasharray: 200,
+    dashoffset: 200,
+    durationMs: 1000,
+    delayMs: 1400
+  },
+  stroke2: {
+    dasharray: 200,
+    dashoffset: 200,
+    durationMs: 1000,
+    delayMs: 2500
+  },
+  stroke3: {
+    dasharray: 200,
+    dashoffset: 200,
+    durationMs: 1000,
+    delayMs: 3600
+  }
+};
+
+const speedScaleChange = 0.1;
 
 class App extends React.Component {
 
@@ -10,14 +40,18 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-      selectedCharacter: uppercaseCharacters[0]
+      selectedCharacter: uppercaseCharacters[0],
+      zoomScale: 1.0,
+      speedScale: 1.0
     };
 
     this.handleCharacterClicked = this.handleCharacterClicked.bind(this);
     this.handleKeyPressed = this.handleKeyPressed.bind(this);
+    this.handleIncreaseSpeed = this.handleIncreaseSpeed.bind(this);
+    this.handleDecreaseSpeed = this.handleDecreaseSpeed.bind(this);
   }
 
-  handleCharacterClicked(character){
+  handleCharacterClicked(character) {
     this.setState({ selectedCharacter: character });
   }
 
@@ -25,28 +59,79 @@ class App extends React.Component {
     // console.log(event);
   }
 
+  handleIncreaseSpeed() {
+    this.setState((prevState) => ({
+      speedScale: prevState.speedScale - speedScaleChange
+    }));
+  }
+
+  handleDecreaseSpeed() {
+    this.setState((prevState) => ({
+      speedScale: prevState.speedScale + speedScaleChange
+    }));
+  }
+
   render() {
     const selectedCharacter = this.state.selectedCharacter;
 
     return (
-      <div className="App" onKeyPress={this.handleKeyPressed}>
+      <div>
+        <div className="App" onKeyPress={this.handleKeyPressed}>
 
-          <div className="selected-character">
-            <svg width="200" height="200" xmlns="http://www.w3.org/2000/svg" >
-              {selectedCharacter.strokes.map((stroke, index) =>
-                <path className={`stroke${index}`} d={stroke.path} key={`${selectedCharacter.id}${index}`}
-                  strokeWidth="10" stroke="black" fill="transparent" strokeLinecap="round" strokeLinejoin="round"/>)}
-            </svg>
+          <div className="TopHalf">
+
+            <div>
+              <div>
+                <ButtonGroup orientation="vertical" color="primary">
+                  <Button>Z+</Button>
+                  <Button>Z-</Button>
+                </ButtonGroup>
+              </div>
+              <div>
+                <ButtonGroup orientation="vertical" color="primary">
+                  <Button onClick={this.handleIncreaseSpeed}>S+</Button>
+                  <Button onClick={this.handleDecreaseSpeed}>S-</Button>
+                </ButtonGroup>
+              </div>
+            </div>
+
+            <div className="selected-character">
+              <svg width="200" height="200" xmlns="http://www.w3.org/2000/svg" >
+                {selectedCharacter.strokes.map((stroke, index) => {
+                  const strokeId = `stroke${index}`;
+                  const strokeDasharray = stroke.dasharray ?? DefaultStrokeSettings[strokeId].dasharray;
+                  const strokeDashoffset = stroke.dashoffset ?? DefaultStrokeSettings[strokeId].dashoffset;
+                  const strokeDurationMs = (stroke.durationMs ?? DefaultStrokeSettings[strokeId].durationMs) * this.state.speedScale;
+                  const strokedelayMs = (stroke.delayMs ?? DefaultStrokeSettings[strokeId].delayMs) * this.state.speedScale;
+                  const strokeAnimation = `${strokeDurationMs}ms linear forwards ${strokedelayMs}ms draw`;
+                  return <path d={stroke.path} key={`${selectedCharacter.id}${index}`}
+                    strokeDasharray={strokeDasharray} strokeDashoffset={strokeDashoffset} style={{ animation: strokeAnimation }}
+                    stroke="black" fill="transparent" strokeLinecap="round" strokeLinejoin="round" strokeWidth="15" />;
+                })}
+              </svg>
+            </div>
+
+            <div>
+              <ButtonGroup orientation="vertical" color="primary">
+                <Button>S</Button>
+                <Button>F</Button>
+                <Button>D</Button>
+              </ButtonGroup>
+            </div>
           </div>
-                
+
           <div className="character-selection">
             {uppercaseCharacters.map((character, index) =>
               selectedCharacter === character
                 ? <Button key={index} size="large" color="primary" variant="contained">{character.id}</Button>
                 : <Button key={index} size="large" onClick={() => this.handleCharacterClicked(character)}>{character.id}</Button>
-              )}
+            )}
           </div>
 
+        </div>
+        <div>
+          Footeer
+        </div>
       </div>
     );
   }
